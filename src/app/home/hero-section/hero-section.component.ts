a4ced7407c50a1d0bf6956ed8d8ee8e6293c8625
floren <florendg@gmail.com>
2025-05-04 09:48:15 +0200

    bump to angular 19

--- src/app/home/hero-section/hero-section.component.ts
+++ src/app/home/hero-section/hero-section.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-hero-section',
-  templateUrl: './hero-section.component.html',
+    selector: 'app-hero-section',
+    templateUrl: './hero-section.component.html',
+    standalone: false
 })
 export class HeroSectionComponent {
   image = '../../assets/images/hero-1.png';
